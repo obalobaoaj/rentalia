6f9afe432f9a4d8d7ee67ffad30d25731e956aaa
obalobaoaj <158153334+obalobaoaj@users.noreply.github.com>
2025-06-13 15:43:01 +0800

    last
    Graph

--- core/forms.py
+++ core/forms.py
@@ -80,7 +80,7 @@ class UserRegistrationForm(UserCreationForm):
             'class': 'form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50',
             'placeholder': 'Confirm Password'
         })
-        
+
         # Remove the default password help text
         self.fields['password1'].help_text = ''
         self.fields['password2'].help_text = 'Enter the same password as before, for verification.'
